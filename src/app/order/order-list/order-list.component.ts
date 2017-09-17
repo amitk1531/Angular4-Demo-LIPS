cf780cbdc08ed7a9823e006aaea3b35ded84733c
Ashish Yadav <santosh.yadav198613@gmail.com>
2017-09-17 16:01:28 +0530

    added change detection

--- src/app/order/order-list/order-list.component.ts
+++ src/app/order/order-list/order-list.component.ts
@@ -1,21 +1,29 @@
-import { Component, OnInit, Input ,Output, EventEmitter } from '@angular/core';
+import {
+  Component, OnInit, Input
+  , Output, EventEmitter, OnChanges, SimpleChanges,
+  ChangeDetectionStrategy
+} from '@angular/core';
 import { Order } from '../order'
 
 @Component({
   selector: 'app-order-list',
   templateUrl: './order-list.component.html',
-  styleUrls: ['./order-list.component.css']
+  styleUrls: ['./order-list.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class OrderListComponent implements OnInit {
+export class OrderListComponent implements OnInit, OnChanges {
   @Input() orderList: Order[];
+  @Input() test: string;
   @Output() message = new EventEmitter<string>();
   constructor() { }
 
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log(changes);
+  }
   ngOnInit() {
   }
 
-  sendMessageToParent()
-  {
+  sendMessageToParent() {
     this.message.emit("This is message from child");
   }
 }
--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Order } from './order';
+import { OrderListComponent } from './order-list/order-list.component';
 @Component({
   selector: 'app-order',
   templateUrl: './order.component.html',
@@ -8,15 +9,25 @@ import { Order } from './order';
 export class OrderComponent implements OnInit {
   orders = new Array<Order>();
   message: string;
+  @ViewChild(OrderListComponent)
+  orderListComponet: OrderListComponent;
+
   constructor() { }
 
   ngOnInit() {
     this.orders = [{ id: 1, customerName: 'Test1', orderDate: new Date('aug-16-2007'), qty: 3 },
     { id: 2, customerName: 'Test1', orderDate: new Date('aug-17-2009'), qty: 4 },
     { id: 3, customerName: 'Test1', orderDate: new Date('aug-18-2005'), qty: 5 }]
+
+    // this.orderListComponet.orderList = this.orders;
   }
 
   receiveFromChild(data: string) {
     this.message = data;
+  }
+
+  addOrder() {
+    let order = { id: 4, customerName: 'Test4', orderDate: new Date('aug-17-2017'), qty: 5 };
+    this.orders = [order];
   }
 }
