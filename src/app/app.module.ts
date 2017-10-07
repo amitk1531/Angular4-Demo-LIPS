37bac7f19a10bd6341e8b68e2622b8ce62e12bcc
Ashish Yadav <santosh.yadav198613@gmail.com>
2017-10-07 16:52:12 +0530

    added reactive forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
@@ -20,7 +20,8 @@ import { OrderService } from './service/order/order.service';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [
     //OrderService,
--- src/app/customer/customer.component.ts
+++ src/app/customer/customer.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder, FormGroup,
+  FormControl, Validators, FormArray
+} from '@angular/forms';
 
 @Component({
   selector: 'app-customer',
@@ -8,13 +12,53 @@ import { Component, OnInit } from '@angular/core';
 export class CustomerComponent implements OnInit {
   name: string = 'Test';
   isVisible: boolean = false;
-  constructor() { }
+  customerForm: FormGroup;
+  maxCardsReached: boolean = true;
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
+    this.customerForm = this.fb.group({
+      name: new FormControl('', [Validators.required, Validators.minLength(5)]),
+      age: new FormControl('', [Validators.required]),
+      dob: new FormControl('', [Validators.required]),
+      address: this.fb.group({
+        addressLine1: new FormControl('', [Validators.required, Validators.minLength(5), Validators.maxLength(10)]),
+        addressLine2: new FormControl(''),
+        state: new FormControl('', [Validators.required]),
+        city: new FormControl('', [Validators.required])
+      }),
+      cards: this.fb.array([this.buildForm()])
+    })
   }
 
-  toggle(){
-   this.isVisible = !this.isVisible; 
+  buildForm() {
+    return this.fb.group({
+      cardNumber: new FormControl('', [Validators.required]),
+      expiryMonth: new FormControl('', [Validators.required]),
+      expiryYear: new FormControl('', [Validators.required]),
+      cvv: new FormControl('', [Validators.required])
+    })
+  }
+
+  toggle() {
+    this.isVisible = !this.isVisible;
+  }
+
+  addCard() {
+    const cards = this.customerForm.controls['cards'] as FormArray;
+    if (cards.length <= 4) {
+      this.maxCardsReached = false;
+    }
+    cards.push(this.buildForm());
+  }
+
+  addCustomer() {
+
+  }
+
+  removeCard(i: number) {
+    const cards = this.customerForm.controls['cards'] as FormArray;
+    cards.removeAt(i);
   }
 
 }
